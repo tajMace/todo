94c4972e9accbf98c41c63701cd71c066528fb77
Taj Macé <taj.mac101@gmail.com>
2026-07-26 18:33:49 +1000

    test: added non-empty list test for add

--- src/add/add_test.go
+++ src/add/add_test.go
@@ -14,13 +14,13 @@ func TestAdd(t *testing.T) {
 	t.Run("addition to an empty list", func(t *testing.T) {
 		tasks := types.TaskManager{}
 
-		newTasks := Add(tasks, "todo", "")
-		if len(newTasks.Tasks) == 0 {
-			t.Fatalf("expected item to be added, but wasn't")
+		tasks = Add(tasks, "todo", "")
+		if len(tasks.Tasks) != 1 {
+			t.Fatalf("number of tasks wrong: got %d, wanted %d", len(tasks.Tasks), 1)
 		}
 
 		wantTask := types.Task{
-			ID:   tasks.NextID,
+			ID:   0,
 			Text: "todo",
 			Done: false,
 			Due:  "",
@@ -31,12 +31,50 @@ func TestAdd(t *testing.T) {
 			Tasks:  []types.Task{wantTask},
 		}
 
-		if newTasks.NextID != want.NextID {
-			t.Errorf("NextID wrong: got %d, wanted %d", newTasks.NextID, want.NextID)
+		if tasks.NextID != want.NextID {
+			t.Errorf("NextID wrong: got %d, wanted %d", tasks.NextID, want.NextID)
 		}
 
-		if !reflect.DeepEqual(newTasks.Tasks, want.Tasks) {
-			t.Errorf("Tasks wrong: got %+v, wanted %+v", newTasks.Tasks, want.Tasks)
+		if !reflect.DeepEqual(tasks.Tasks, want.Tasks) {
+			t.Errorf("Tasks wrong: got %+v, wanted %+v", tasks.Tasks, want.Tasks)
+		}
+	})
+
+	t.Run("addition to a non-empty list", func(t *testing.T) {
+		tasks := types.TaskManager{}
+
+		wantTask1 := types.Task{
+			ID:   0,
+			Text: "todo",
+			Done: false,
+			Due:  "",
+		}
+		wantTask2 := types.Task{
+			ID:   1,
+			Text: "todo2",
+			Done: false,
+			Due:  "",
+		}
+
+		want := types.TaskManager{
+			NextID: 2,
+			Tasks:  []types.Task{wantTask1, wantTask2},
+		}
+
+		tasks = Add(tasks, "todo", "")
+		tasks = Add(tasks, "todo2", "")
+
+		if len(tasks.Tasks) != 2 {
+			t.Logf("got %+v, wanted %+v", tasks, want)
+			t.Fatalf("number of tasks wrong: got %d, wanted %d", len(tasks.Tasks), 2)
+		}
+
+		if tasks.NextID != want.NextID {
+			t.Errorf("NextID wrong: got %d, wanted %d", tasks.NextID, want.NextID)
+		}
+
+		if !reflect.DeepEqual(tasks.Tasks, want.Tasks) {
+			t.Errorf("Tasks wrong: got %+v, wanted %+v", tasks.Tasks, want.Tasks)
 		}
 	})
 }
