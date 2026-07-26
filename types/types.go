96e44b4f7f90d67a121404443326d162ba70055b
Taj Macé <taj.mac101@gmail.com>
2026-07-26 21:39:30 +1000

    feat: included 'constructor' functions

--- types/types.go
+++ types/types.go
@@ -3,14 +3,30 @@
 
 package types
 
+/* ===== TaskManager + Helpers ====== */
 type TaskManager struct {
 	NextID int    `json:"next_id"`
 	Tasks  []Task `json:"tasks"`
 }
 
+func NewTaskManager(tasks []Task) TaskManager {
+	return TaskManager{NextID: len(tasks), Tasks: tasks}
+}
+
+/* ===== Task + Helpers ===== */
 type Task struct {
 	ID   int    `json:"id"`
 	Text string `json:"text"`
 	Due  string `json:"due"`
 	Done bool   `json:"done"`
+}
+
+// constructor - always sets done to 'false'
+func NewTask(id int, text, due string) Task {
+	return Task{
+		ID:   id,
+		Text: text,
+		Due:  due,
+		Done: false,
+	}
 }
