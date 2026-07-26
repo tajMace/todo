ccc39bce3a3ffcd97c803a02010bb126f3702a98
Taj Macé <taj.mac101@gmail.com>
2026-07-26 21:04:08 +1000

    feat: added error checking to Add

--- src/add/add_test.go
+++ src/add/add_test.go
@@ -14,7 +14,11 @@ func TestAdd(t *testing.T) {
 	t.Run("addition to an empty list", func(t *testing.T) {
 		tasks := types.TaskManager{}
 
-		tasks = Add(tasks, "todo", "")
+		tasks, err := Add(tasks, "todo", "")
+		if err != nil {
+			t.Fatalf("got an error, but didn't expect one")
+		}
+
 		if len(tasks.Tasks) != 1 {
 			t.Fatalf("number of tasks wrong: got %d, wanted %d", len(tasks.Tasks), 1)
 		}
@@ -41,8 +45,6 @@ func TestAdd(t *testing.T) {
 	})
 
 	t.Run("addition to a non-empty list", func(t *testing.T) {
-		tasks := types.TaskManager{}
-
 		wantTask1 := types.Task{
 			ID:   0,
 			Text: "todo",
@@ -53,7 +55,7 @@ func TestAdd(t *testing.T) {
 			ID:   1,
 			Text: "todo2",
 			Done: false,
-			Due:  "",
+			Due:  "2026-07-26",
 		}
 
 		want := types.TaskManager{
@@ -61,8 +63,15 @@ func TestAdd(t *testing.T) {
 			Tasks:  []types.Task{wantTask1, wantTask2},
 		}
 
-		tasks = Add(tasks, "todo", "")
-		tasks = Add(tasks, "todo2", "")
+		tasks, err := Add(types.TaskManager{}, "todo", "")
+		if err != nil {
+			t.Fatalf("got an error, but didn't expect one")
+		}
+
+		tasks, err = Add(tasks, "todo2", "2026-07-26")
+		if err != nil {
+			t.Fatalf("got an error, but didn't expect one")
+		}
 
 		if len(tasks.Tasks) != 2 {
 			t.Logf("got %+v, wanted %+v", tasks, want)
@@ -75,6 +84,19 @@ func TestAdd(t *testing.T) {
 
 		if !reflect.DeepEqual(tasks.Tasks, want.Tasks) {
 			t.Errorf("Tasks wrong: got %+v, wanted %+v", tasks.Tasks, want.Tasks)
+		}
+	})
+
+	t.Run("incorrectly formatted date", func(t *testing.T) {
+		_, err := Add(types.TaskManager{}, "todo", "not-a-date")
+		want := ErrIncorrectDateFormat
+
+		if err == nil {
+			t.Fatal("Expected an error, but didn't get one")
+		}
+
+		if err != want {
+			t.Errorf("Error wrong: got %+v, wanted %+v", err, want)
 		}
 	})
 }
