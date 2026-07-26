015c6dd6ca26d6b652b0e168742092ac8d1f7c7c
Taj Macé <taj.mac101@gmail.com>
2026-07-26 21:39:05 +1000

    refac: refactored add tests

--- src/add/add_test.go
+++ src/add/add_test.go
@@ -12,91 +12,78 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Run("addition to an empty list", func(t *testing.T) {
-		tasks := types.TaskManager{}
+		wantTask := types.NewTask(0, "todo", "")
+		want := types.NewTaskManager([]types.Task{wantTask})
 
-		tasks, err := Add(tasks, "todo", "")
-		if err != nil {
-			t.Fatalf("got an error, but didn't expect one")
-		}
+		got := addAndAssertNoErrors(t, types.TaskManager{}, "todo", "")
 
-		if len(tasks.Tasks) != 1 {
-			t.Fatalf("number of tasks wrong: got %d, wanted %d", len(tasks.Tasks), 1)
-		}
-
-		wantTask := types.Task{
-			ID:   0,
-			Text: "todo",
-			Done: false,
-			Due:  "",
-		}
-
-		want := types.TaskManager{
-			NextID: 1,
-			Tasks:  []types.Task{wantTask},
-		}
-
-		if tasks.NextID != want.NextID {
-			t.Errorf("NextID wrong: got %d, wanted %d", tasks.NextID, want.NextID)
-		}
-
-		if !reflect.DeepEqual(tasks.Tasks, want.Tasks) {
-			t.Errorf("Tasks wrong: got %+v, wanted %+v", tasks.Tasks, want.Tasks)
-		}
+		compareTasks(t, got, want)
 	})
 
 	t.Run("addition to a non-empty list", func(t *testing.T) {
-		wantTask1 := types.Task{
-			ID:   0,
-			Text: "todo",
-			Done: false,
-			Due:  "",
-		}
-		wantTask2 := types.Task{
-			ID:   1,
-			Text: "todo2",
-			Done: false,
-			Due:  "2026-07-26",
-		}
+		wantTask1 := types.NewTask(0, "todo", "")
+		wantTask2 := types.NewTask(1, "todo2", "2026-07-26")
+		want := types.NewTaskManager([]types.Task{wantTask1, wantTask2})
 
-		want := types.TaskManager{
-			NextID: 2,
-			Tasks:  []types.Task{wantTask1, wantTask2},
-		}
+		got := types.TaskManager{}
+		got = addAndAssertNoErrors(t, got, "todo", "")
+		got = addAndAssertNoErrors(t, got, "todo2", "2026-07-26")
 
-		tasks, err := Add(types.TaskManager{}, "todo", "")
-		if err != nil {
-			t.Fatalf("got an error, but didn't expect one")
-		}
-
-		tasks, err = Add(tasks, "todo2", "2026-07-26")
-		if err != nil {
-			t.Fatalf("got an error, but didn't expect one")
-		}
-
-		if len(tasks.Tasks) != 2 {
-			t.Logf("got %+v, wanted %+v", tasks, want)
-			t.Fatalf("number of tasks wrong: got %d, wanted %d", len(tasks.Tasks), 2)
-		}
-
-		if tasks.NextID != want.NextID {
-			t.Errorf("NextID wrong: got %d, wanted %d", tasks.NextID, want.NextID)
-		}
-
-		if !reflect.DeepEqual(tasks.Tasks, want.Tasks) {
-			t.Errorf("Tasks wrong: got %+v, wanted %+v", tasks.Tasks, want.Tasks)
-		}
+		compareTasks(t, got, want)
 	})
 
 	t.Run("incorrectly formatted date", func(t *testing.T) {
-		_, err := Add(types.TaskManager{}, "todo", "not-a-date")
 		want := ErrIncorrectDateFormat
 
-		if err == nil {
-			t.Fatal("Expected an error, but didn't get one")
-		}
+		err := addAndAssertError(t, types.TaskManager{}, "todo", "not-a-date")
 
-		if err != want {
-			t.Errorf("Error wrong: got %+v, wanted %+v", err, want)
-		}
+		compareErrors(t, err, want)
 	})
+}
+
+/* ========== helpers ========== */
+func addAndAssertNoErrors(t testing.TB, tasks types.TaskManager, text, due string) types.TaskManager {
+	t.Helper()
+
+	got, err := Add(tasks, text, due)
+	if err != nil {
+		t.Fatalf("got an error, but didn't expect one")
+	}
+
+	return got
+}
+
+func addAndAssertError(t testing.TB, tasks types.TaskManager, text, due string) error {
+	t.Helper()
+
+	_, err := Add(tasks, text, due)
+	if err == nil {
+		t.Fatal("Expected an error, but didn't get one")
+	}
+
+	return err
+}
+
+func compareTasks(t testing.TB, got, want types.TaskManager) {
+	t.Helper()
+
+	if len(got.Tasks) != len(want.Tasks) {
+		t.Fatalf("number of tasks wrong: got %d, wanted %d", len(got.Tasks), len(want.Tasks))
+	}
+
+	if got.NextID != want.NextID {
+		t.Errorf("NextID wrong: got %d, wanted %d", got.NextID, want.NextID)
+	}
+
+	if !reflect.DeepEqual(got.Tasks, want.Tasks) {
+		t.Errorf("Tasks wrong: got %+v, wanted %+v", got.Tasks, want.Tasks)
+	}
+}
+
+func compareErrors(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("Error wrong: got %+v, wanted %+v", got, want)
+	}
 }
